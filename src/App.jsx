11f30679ff98c5453a3a8e803370db56aadd5066
Tejas <tejashinde4046@gmail.com>
2024-01-26 19:15:02 +0530

    Revert "Initial Commit"
    This reverts commit d5cb309754aabfa18bdc91ddf17c7929f8d7250f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import TicTacToe from './components/TicTacToe'
 
 function App() {
+  
+
   return (
-    <div className='flex flex-col items-center bg-gray-900 text-white h-screen  '>
-      <TicTacToe />
+    <div className='text-center'>
+      Hello World
     </div>
   )
 }
